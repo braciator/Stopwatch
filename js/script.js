982698b96747ecbfa9e3c2cfa853c9e15c160a09
Daniel <kuchta.daniel1@gmail.com>
2024-06-09 23:50:51 +0200

    Added Modal

--- js/script.js
+++ js/script.js
@@ -1,89 +1,97 @@
-const startBtn = document.querySelector('.start')
-const pauseBtn = document.querySelector('.pause')
-const stopBtn = document.querySelector('.stop')
-const resetBtn = document.querySelector('.reset')
-const historyBtn = document.querySelector('.history')
-const stopwatch = document.querySelector('.stopwatch')
-const time = document.querySelector('.time')
-const timeList = document.querySelector('.time-list')
+const startBtn = document.querySelector(".start")
+const pauseBtn = document.querySelector(".pause")
+const stopBtn = document.querySelector(".stop")
+const resetBtn = document.querySelector(".reset")
+const historyBtn = document.querySelector(".history")
+const stopwatch = document.querySelector(".stopwatch")
+const time = document.querySelector(".time")
+const timeList = document.querySelector(".time-list")
 
-const infoBtn = document.querySelector('.info')
-const modalShadow = document.querySelector('.modal-shadow')
-const closeModalBtn = document.querySelector('.close')
+const infoBtn = document.querySelector(".info")
+const modalShadow = document.querySelector(".modal-shadow")
+const closeModalBtn = document.querySelector(".close")
 
-let countTime;
-let minutes = 0;
-let seconds = 0;
+let countTime
+let minutes = 0
+let seconds = 0
 
-let timesArr = [];
+let timesArr = []
 
 const handleStart = () => {
-    clearInterval(countTime);
+	clearInterval(countTime)
 
-    countTime = setInterval(() => {
-
-        if (seconds < 9) {
-            seconds++;
-            stopwatch.textContent = `${minutes}:0${seconds}`
-        } else if (seconds >= 9 && seconds < 59) {
-            seconds++;
-            stopwatch.textContent = `${minutes}:${seconds}`
-        } else {
-            minutes++;
-            seconds = 0;
-            stopwatch.textContent = `${minutes}:00`
-        }
-        
-    }, 1000)
-
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++
+			stopwatch.textContent = `${minutes}:0${seconds}`
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++
+			stopwatch.textContent = `${minutes}:${seconds}`
+		} else {
+			minutes++
+			seconds = 0
+			stopwatch.textContent = `${minutes}:00`
+		}
+	}, 1000)
 }
 
 const handlePause = () => {
-    clearInterval(countTime);
+	clearInterval(countTime)
 }
 
 const handleStop = () => {
-    
-    time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`
+	time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`
 
-    if (stopwatch.textContent !== '0:00') {
-        time.style.visibility = 'visible';
-        timesArr.push(stopwatch.textContent)
-    }
-    
-    clearStuff();
+	if (stopwatch.textContent !== "0:00") {
+		time.style.visibility = "visible"
+		timesArr.push(stopwatch.textContent)
+	}
+
+	clearStuff()
 }
 
 const handleReset = () => {
-    time.style.visibility = 'hidden';
-    timesArr = [];
-    clearStuff();
+	time.style.visibility = "hidden"
+	timesArr = []
+	clearStuff()
 }
 
 const clearStuff = () => {
-    clearInterval(countTime);
-    stopwatch.textContent = '0:00';
-    timeList.textContent = '';
-    seconds = 0;
-    minutes = 0;
+	clearInterval(countTime)
+	stopwatch.textContent = "0:00"
+	timeList.textContent = ""
+	seconds = 0
+	minutes = 0
 }
 
 const showHistory = () => {
-    timeList.textContent = '';
-    let num = 1;
+	timeList.textContent = ""
+	let num = 1
 
-    timesArr.forEach(time => {
-        const newTime = document.createElement('li');
-        newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`
+	timesArr.forEach(time => {
+		const newTime = document.createElement("li")
+		newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`
 
-        timeList.appendChild(newTime)
-        num++;
-    })
+		timeList.appendChild(newTime)
+		num++
+	})
 }
 
-startBtn.addEventListener('click', handleStart)
-pauseBtn.addEventListener('click', handlePause)
-stopBtn.addEventListener('click', handleStop)
-resetBtn.addEventListener('click', handleReset)
-historyBtn.addEventListener('click', showHistory)
+const showModal = () => {
+	if (!(modalShadow.style.display === "block")) {
+		modalShadow.style.display = "block"
+	} else {
+		modalShadow.style.display = "none"
+	};
 
+    modalShadow.classList.toggle('modal-animation')
+}
+
+startBtn.addEventListener("click", handleStart)
+pauseBtn.addEventListener("click", handlePause)
+stopBtn.addEventListener("click", handleStop)
+resetBtn.addEventListener("click", handleReset)
+historyBtn.addEventListener("click", showHistory)
+infoBtn.addEventListener("click", showModal)
+closeModalBtn.addEventListener("click", showModal)
+window.addEventListener('click', e => e.target === modalShadow ? showModal() : false)
